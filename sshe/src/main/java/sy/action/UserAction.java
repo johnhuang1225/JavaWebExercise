e6d765a0095109dec37b9e0f04d32ab5462c0341
johnhuang <johnhuang1225@gmail.com>
2015-01-28 15:34:17 +0800

    Spring Autowired

--- sshe/src/main/java/sy/action/UserAction.java
+++ sshe/src/main/java/sy/action/UserAction.java
@@ -5,6 +5,7 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -16,11 +17,22 @@ import sy.service.UserServiceI;
 public class UserAction {
 
 	private static final Logger logger = Logger.getLogger(UserAction.class);
+	private UserServiceI userService;
+
+	public UserServiceI getUserService() {
+		return userService;
+	}
+
+	@Autowired
+	public void setUserService(UserServiceI userService) {
+		this.userService = userService;
+	}
 
 	public void test() {
-		logger.info("進入UserAction.test()...");
-		ApplicationContext ac = WebApplicationContextUtils.getWebApplicationContext(ServletActionContext.getServletContext());
-		UserServiceI userService = (UserServiceI)ac.getBean("userService");
+		logger.info("使用Spring Autowired...");
+//		ApplicationContext ac =
+//				WebApplicationContextUtils.getWebApplicationContext(ServletActionContext.getServletContext());
+//		UserServiceI userService = (UserServiceI)ac.getBean("userService");
 		userService.save();
 	}
 }
